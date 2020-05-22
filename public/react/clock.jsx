c5498868b6191349b4e3ddaa2be3d259c29cd29c
liam-king-99 <51165020+liam-king-99@users.noreply.github.com>
2020-05-22 17:56:06 -0400

    Clock page only shows time portion of date

--- public/react/clock.jsx
+++ public/react/clock.jsx
@@ -1,7 +1,7 @@
 function showTime() {
     const newTimeValue = (
         <div>
-            <p>{new Date().toLocaleString()}</p>
+            <p>{new Date().toLocaleTimeString()}</p>
         </div>
     )
     ReactDOM.render(newTimeValue, document.getElementById("clockDiv"));
